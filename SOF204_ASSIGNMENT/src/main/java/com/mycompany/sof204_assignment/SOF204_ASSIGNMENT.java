21387543e92b6715bea5f55d7fdc03ec629fa651
NguyenHieu9999 <144803709+NguyenHieu9999@users.noreply.github.com>
2023-09-26 20:47:19 +0700

    Add files via upload

---
+++ SOF204_ASSIGNMENT/src/main/java/com/mycompany/sof204_assignment/SOF204_ASSIGNMENT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sof204_assignment;
+
+/**
+ *
+ * @author Admin
+ */
+public class SOF204_ASSIGNMENT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
